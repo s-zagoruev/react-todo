bfb05be48af2054e3ea88e84c9054e23b2880365
s-zagoruev <zagoruev@gmail.com>
2021-06-02 01:15:18 +0300

    Add onLabelClick

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -2,10 +2,12 @@ import React, {Component} from 'react'
 import './todo-list-item.css'
 
 export default class TodoListItem extends Component {
+  onLabelClick = () => {
+    console.log(`Done: ${this.props.label}`)
+  }
 
   render() {
     const {label, important = false} = this.props;
-
     const style = {
       color: important ? 'tomato' : 'black'
     }
@@ -14,7 +16,8 @@ export default class TodoListItem extends Component {
       <span className="todo-list-item">
       <span
         className="todo-list-item-label"
-        style={style}>
+        style={style}
+        onClick={this.onLabelClick}>
         {label}
           </span>
 
